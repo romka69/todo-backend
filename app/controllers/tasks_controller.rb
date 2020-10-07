7cd02f0eda2ae126fd92999df173c7e2c34e66b3
roman <romka69tver@gmail.com>
2020-10-07 22:08:27 +0300

    add tasks#create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,15 @@ class TasksController < ApplicationController
     @tasks = Task.all
     render json: @tasks, status: :ok
   end
+
+  def create
+    @task = Task.create!(task_params)
+    render json: @task, status: :created
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
 end
